7ce43f7fa1b103fb173ae7980e4dfababb061f21
md-faysal-numin <mdfaysalnumin@gmail.com>
2025-09-18 18:53:43 +0600

    Fix some bug

--- cart.js
+++ cart.js
@@ -1,34 +1,45 @@
-const params = new URLSearchParams(window.location.search);
-const namee = params.get("name");
-const email = params.get("email");
-let nav = document.querySelector(".navbar-brand");
-nav.textContent+=namee;
+let email = sessionStorage.getItem("isLoggedIn") ;
 
-let logoutBtn = document.querySelector("#logoutBtn");
-let cartBtn = document.querySelector("#cartBtn");
+if(email !==null){
+    
+    let  namee = JSON.parse(localStorage.getItem(email)).name;
 
-let cartList = document.querySelector(".cartList");
+    let nav = document.querySelector(".navbar-brand");
+    nav.textContent+=namee;
+    
+    let logoutBtn = document.querySelector("#logoutBtn");
+    let cartBtn = document.querySelector("#cartBtn");
+    
+    let cartList = document.querySelector(".cartList");
+    
+    const obj  = JSON.parse(localStorage.getItem(email));
+    console.log(obj.cart);
+    
+    Object.entries(obj.cart).forEach( ([k,v]) =>{
+        let li = document.createElement('li');
+        li.classList.add("list-group-item");
+    
+        li.textContent = `${v.title} - $${v.price} x ${v.quantity}`;
+        cartList.appendChild(li);
+    
+    })
+    
+    
+    dashBtn.addEventListener("click",()=>{
+         setTimeout(()=>{
+            window.location.href = `dashboard.html`;
+        },1000)
+    })
+    logoutBtn.addEventListener("click",()=>{
+            setTimeout(()=>{
+                (function logout() {
+          sessionStorage.clear();
+          console.log("Session cleared. User logged out.");
+        })();
+            window.location.href = `login.html`;
+        },1000)
+    })
+}
+else{
 
-const obj  = JSON.parse(localStorage.getItem(email));
-console.log(obj.cart);
-
-Object.entries(obj.cart).forEach( ([k,v]) =>{
-    let li = document.createElement('li');
-    li.classList.add("list-group-item");
-
-    li.textContent = `${v.title} - $${v.price} x ${v.quantity}`;
-    cartList.appendChild(li);
-
-})
-
-
-dashBtn.addEventListener("click",()=>{
-     setTimeout(()=>{
-        window.location.href = `dashboard.html?email=${email}&name=${JSON.parse(localStorage.getItem(email)).name}`;
-    },1000)
-})
-logoutBtn.addEventListener("click",()=>{
-        setTimeout(()=>{
-        window.location.href = `login.html`;
-    },1000)
-})
+}
--- dashboard.js
+++ dashboard.js
@@ -2,9 +2,9 @@ let email = sessionStorage.getItem("isLoggedIn") ;
 let namee ;
 if (email !== null) {
     namee = JSON.parse(localStorage.getItem(email)).name;
-  console.log(`${email} is logged in!`);
-}
-console.log(email,namee);
+
+
+
 
 let logoutBtn = document.querySelector("#logoutBtn");
 let cartBtn = document.querySelector("#cartBtn");
@@ -76,3 +76,50 @@ logoutBtn.addEventListener("click", () => {
     window.location.href = `login.html`;
   }, 1000);
 });
+
+}
+else
+{
+
+
+  console.log("object");
+let container = document.querySelector(".container");
+  fetch("https://dummyjson.com/products")
+  .then((res) => res.json())
+  .then((data) => {
+    for (let i = 0; i < 10; i++) {
+      let p = data.products[i];
+      container.innerHTML += `
+
+<div class="card" style="width: 18rem">
+      <img src="${p.images[0]}" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">${p.title}</h5>
+   
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Price: ${p.price}</li>
+            <li class="list-group-item">Catagory: ${p.category}</li>
+            <li class="list-group-item">Rating: ${p.rating}</li>
+        </ul>
+         <a href="#" 
+     onclick="addToCart(${p.id}, '${p.title}', ${p.price})" 
+     class="btn btn-primary">
+    Add to cart
+  </a>
+      </div>
+    </div>
+
+
+
+        `;
+    }
+  });
+
+
+
+  let logoutBtn = document.querySelector("#logoutBtn");
+let cartBtn = document.querySelector("#cartBtn");
+logoutBtn.style.visibility ="hidden";
+cartBtn.style.visibility ="hidden";
+
+}
